ddfd35794a327024232f7d06b864f219c2e8a0a9
purna16 <shivar640@gmail.com>
2023-06-02 17:24:27 +0530

    test6

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -3,7 +3,7 @@ import React from 'react';
 // import { render } from "@testing-library/react";
 import {Link} from "react-router-dom";
 import React1 from "../assets/iitb.png";
-import React2 from "../assets/devops.png";
+import React2 from "../assets/ci-cd.png";
 
 const AboutContent = () => {
     // render()
